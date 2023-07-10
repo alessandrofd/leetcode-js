d15734ab24d863617f8b5b47e0ac47277d4a49c7
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-07-10 11:49:43 -0300

    1502. Can make arithmetic progression from sequence

---
+++ 1502_-can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,36 @@
+/**
+ * A sequence of numbers is called an arithmetic progression if the difference
+ * between any two consecutive elements is the same.
+ *
+ * Given an array of numbers arr, return true if the array can be rearranged to
+ * form an arithmetic progression. Otherwise, return false.
+ *
+ * Constraints:
+ *    2 <= arr.length <= 1000
+ *    -10^6 <= arr[i] <= 10^6
+ */
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const canMakeArithmeticProgression = (arr) => {
+  const n = arr.length
+  arr.sort((a, b) => a - b)
+  const diff = arr[1] - arr[0]
+  for (let i = 2; i < n; i++) {
+    if (arr[i] - arr[i - 1] !== diff) {
+      return false
+    }
+  }
+
+  return true
+}
+
+arr = [3, 5, 1]
+// Expected: true
+
+// arr = [1, 2, 4]
+// Expected: false
+
+console.log(canMakeArithmeticProgression(arr))
---
+++ 1502b-can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,25 @@
+/**
+ * A sequence of numbers is called an arithmetic progression if the difference
+ * between any two consecutive elements is the same.
+ *
+ * Given an array of numbers arr, return true if the array can be rearranged to
+ * form an arithmetic progression. Otherwise, return false.
+ *
+ * Constraints:
+ *    2 <= arr.length <= 1000
+ *    -10^6 <= arr[i] <= 10^6
+ */
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const canMakeArithmeticProgression = (arr) => {}
+
+arr = [3, 5, 1]
+// Expected: true
+
+// arr = [1, 2, 4]
+// Expected: false
+
+console.log(canMakeArithmeticProgression(arr))
