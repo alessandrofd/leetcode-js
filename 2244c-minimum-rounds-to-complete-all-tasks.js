26f57a24cb30b3bb666208501b93a96e4432bebe
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-07-10 22:30:21 -0300

    2244c. Minimum rounds to complete all tasks

--- 2244c-minimum-rounds-to-complete-all-tasks.js
+++ 2244c-minimum-rounds-to-complete-all-tasks.js
@@ -15,15 +15,30 @@
  * @param {number[]} tasks
  * @return {number}
  */
-const minimumRounds = (tasks) => {}
+const minimumRounds = (tasks) => {
+  const n = tasks.length
+  const freqs = new Map()
+
+  for (const task of tasks) {
+    freqs.set(task, (freqs.get(task) ?? 0) + 1)
+  }
+
+  let rounds = 0
+  for (const [task, freq] of freqs) {
+    if (freq === 1) return -1
+    rounds += Math.ceil(freq / 3)
+  }
+
+  return rounds
+}
 
 tasks = [2, 2, 3, 3, 2, 4, 4, 4, 4, 4]
-Output: 4
+// Output: 4
 
-// tasks = [2, 3, 3]
-Output: -1
+tasks = [2, 3, 3]
+// Output: -1
 
-// tasks = [5, 5, 5, 5]
+tasks = [5, 5, 5, 5]
 // Output: 2
 
 console.log(minimumRounds(tasks))
