01e95d52ff5f3f26f4ec516fe84a482ecdc47b43
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-07-16 23:57:40 -0300

    1232a. Check if it is a straight line

--- 1232a-check-if-it-is-a-straight-line.js
+++ 1232a-check-if-it-is-a-straight-line.js
@@ -19,22 +19,37 @@
  * @param {number[][]} coordinates
  * @return {boolean}
  */
-const checkStraightLine = (coordinates) => {}
+const checkStraightLine = (coordinates) => {
+  const n = coordinates.length
+
+  if (n === 2) return true
+
+  const [x0, y0] = coordinates[0]
+  const [x1, y1] = coordinates[1]
+  const [deltaX, deltaY] = [x1 - x0, y1 - y0]
+
+  for (let i = 2; i < n; i++) {
+    const [xi, yi] = coordinates[i]
+    if (deltaX * (yi - y0) !== deltaY * (xi - x0)) return false
+  }
+
+  return true
+}
 
 // prettier-ignore
 coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
 // Expected: true
 
 // prettier-ignore
-// coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
 // Expected: false
 
 // prettier-ignore
-// coordinates = [[1,2],[1,3],[1,4],[1,5],[1,6],[6,7]]
+coordinates = [[1,2],[1,3],[1,4],[1,5],[1,6],[6,7]]
 // Expected: false
 
 // prettier-ignore
-// coordinates = [[-3,-2],[-1,-2],[2,-2],[-2,-2],[0,-2]]
+coordinates = [[-3,-2],[-1,-2],[2,-2],[-2,-2],[0,-2]]
 // Expected: true
 
 console.log(checkStraightLine(coordinates))
