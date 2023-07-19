2d075846187cd1500c221507884f49005fea52f7
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-07-18 22:30:27 -0300

    373a. Find k paris with smallest sums

--- 373a-find-k-pairs-with-smallest-sums.js
+++ 373a-find-k-pairs-with-smallest-sums.js
@@ -29,13 +29,32 @@ import {
  * @param {number} k
  * @return {number[][]}
  */
-const kSmallestPairs_kSquared = (nums1, nums2, k) => {}
+const kSmallestPairs_kSquared = (nums1, nums2, k) => {
+  const queue = new MaxPriorityQueue({ priority: (pair) => pair[0] + pair[1] })
+
+  for (const n1 of nums1) {
+    for (const n2 of nums2) {
+      queue.enqueue([n1, n2])
+      if (queue.size() > k) queue.dequeue()
+    }
+  }
+
+  const result = []
+  while (queue.size() > 0) {
+    result.push(queue.dequeue().element)
+  }
+
+  return result
+}
 
 // Utiliza uma fila de prioridades min. Seleciona-se o primeiro elemento da fila,
 // caracterizado pelos índices de cada um dos vetores nums1 e nums1, e insere
 // a tupla na resposta. Avança os índices um de cada vez e enfileira
 // a combinação caso já não tenha sido. Essas operação são repetidas k vezes ou
 // até que a fila se esvazie.
+
+// Lembrar-se de que os vetores estão ordenados, logo [0,0] sempre será o menor
+// valor possível
 
 // Estourou a pilha de memória
 
@@ -45,7 +64,39 @@ const kSmallestPairs_kSquared = (nums1, nums2, k) => {}
  * @param {number} k
  * @return {number[][]}
  */
-const kSmallestPairs_stepByStep = (nums1, nums2, k) => {}
+const kSmallestPairs_stepByStep = (nums1, nums2, k) => {
+  const n1 = nums1.length
+  const n2 = nums2.length
+
+  const queue = new MinPriorityQueue({
+    priority: (idxs) => nums1[idxs[0]] + nums2[idxs[1]],
+  })
+
+  const visited = Array(n1)
+    .fill()
+    .map((_) => Array(n2).fill(false))
+
+  queue.enqueue([0, 0])
+  visited[0][0] = true
+
+  const result = []
+  while (result.length < k && queue.size() > 0) {
+    const [idx1, idx2] = queue.dequeue().element
+    result.push([nums1[idx1], nums2[idx2]])
+
+    if (idx1 + 1 < n1 && !visited[idx1 + 1][idx2]) {
+      queue.enqueue([idx1 + 1, idx2])
+      visited[idx1 + 1][idx2] = true
+    }
+
+    if (idx2 + 1 < n2 && !visited[idx1][idx2 + 1]) {
+      queue.enqueue([idx1, idx2 + 1])
+      visited[idx1][idx2 + 1] = true
+    }
+  }
+
+  return result
+}
 
 // Otimização da solução anterior
 
@@ -66,7 +117,27 @@ const kSmallestPairs_stepByStep = (nums1, nums2, k) => {}
  * @param {number} k
  * @return {number[][]}
  */
-const kSmallestPairs_gangorra = (nums1, nums2, k) => {}
+const kSmallestPairs_gangorra = (nums1, nums2, k) => {
+  const n1 = nums1.length
+  const n2 = nums2.length
+
+  const queue = new MinPriorityQueue({
+    priority: (idxs) => nums1[idxs[0]] + nums2[idxs[1]],
+  })
+  queue.enqueue([0, 0])
+
+  const result = []
+  while (result.length < k && queue.size() > 0) {
+    const [idx1, idx2] = queue.dequeue().element
+    result.push([nums1[idx1], nums2[idx2]])
+
+    if (idx1 + 1 < n1 && idx2 === 0) queue.enqueue([idx1 + 1, idx2])
+
+    if (idx2 + 1 < n2) queue.enqueue([idx1, idx2 + 1])
+  }
+
+  return result
+}
 
 let nums1 = [1, 7, 11]
 let nums2 = [2, 4, 6]
