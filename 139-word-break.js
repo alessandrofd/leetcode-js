11202c67ca05e907d42894959594c67ef684d0ae
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-08-04 07:45:08 -0300

    139. Word break

--- 139-word-break.js
+++ 139-word-break.js
@@ -1,23 +1,25 @@
-/*
-Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+/**
+ * Given a string s and a dictionary of strings wordDict, return true if s can
+ * be segmented into a space-separated sequence of one or more dictionary words.
+ *
+ * Note that the same word in the dictionary may be reused multiple times in the
+ * segmentation.
+ *
+ * Constraints:
+ *    1 <= s.length <= 300
+ *    1 <= wordDict.length <= 1000
+ *    1 <= wordDict[i].length <= 20
+ *    s and wordDict[i] consist of only lowercase English letters.
+ *    All the strings of wordDict are unique.
+ */
 
-Note that the same word in the dictionary may be reused multiple times in the segmentation.
-
-
-Constraints:
-    1 <= s.length <= 300
-    1 <= wordDict.length <= 1000
-    1 <= wordDict[i].length <= 20
-    s and wordDict[i] consist of only lowercase English letters.
-    All the strings of wordDict are unique.
-*/
+// Approach 1: Brute Force - TLE
 
 /**
  * @param {string} s
  * @param {string[]} wordDict
  * @return {boolean}
  */
-// Approach 1: Brute Force - TLE
 const wordBreak_bruteForce = (s, wordDict) => {
   const set = new Set(wordDict)
 
@@ -33,6 +35,12 @@ const wordBreak_bruteForce = (s, wordDict) => {
 }
 
 // Approach 2: Recursion with memoization
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
 const wordBreak_recursionMemoization = (s, wordDict) => {
   const n = s.length
   const set = new Set(wordDict)
@@ -53,6 +61,12 @@ const wordBreak_recursionMemoization = (s, wordDict) => {
 }
 
 // Approach 3: Using Breadth-First-Search
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
 const wordBreak_BFS = (s, wordDict) => {
   const set = new Set(wordDict)
   const visited = new Set()
@@ -72,10 +86,29 @@ const wordBreak_BFS = (s, wordDict) => {
   return false
 }
 
-// Approach 4: Using Dynamic Programming
-/*
-A programação dinâmica é construída tendo como parâmentro o comprimento das substrings que atendem os requisitos do problema. A partir da substring de comprimento 0 - que, por definição, atende ao enunciado do problema, percorremos todo o comprimento da string. A cada passo verificamos se há alguma combinação de substrings anteriores que podem ser compostas a partir das palavras do dicionário e substrings que correspondam a alguma palavra do dicionário. Caso estas condições sejam atendidas, consideramos que a substring de comprimento que estamos analisando atende ao enunciado do problemas e esta se torna candidata a compor nova substring válida nas próximas iterações. Ao fim nos interessa saber se a string de comprimento igual à string original, ou seja, a própria string original é válida, pois pode ser formada por substrings menores consideradas válidas em iterações anteriores e alguma palavra no dicionário.
-*/
+/**
+ * Approach 4: Using Dynamic Programming
+ *
+ * A programação dinâmica é construída tendo como parâmentro o comprimento das
+ * substrings que atendem os requisitos do problema. A partir da substring de
+ * comprimento 0 - que, por definição, atende ao enunciado do problema,
+ * percorremos todo o comprimento da string. A cada passo verificamos se há
+ * alguma combinação de substrings anteriores que podem ser compostas a partir
+ * das palavras do dicionário e substrings que correspondam a alguma palavra do
+ * dicionário. Caso estas condições sejam atendidas, consideramos que a
+ * substring de comprimento que estamos analisando atende ao enunciado do
+ * problemas e esta se torna candidata a compor nova substring válida nas
+ * próximas iterações. Ao fim nos interessa saber se a string de comprimento
+ * igual à string original, ou seja, a própria string original é válida, pois
+ * pode ser formada por substrings menores consideradas válidas em iterações
+ * anteriores e alguma palavra no dicionário.
+ */
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
 const wordBreak_DP = (s, wordDict) => {
   const n = s.length
   const set = new Set(wordDict)
