60c5a4ed5df9ad444cc428e31409518535e429a8
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-08-13 21:57:43 -0300

    215. Kth largest element in an array

---
+++ 215-kth-largest-element-in-an-array.js
@@ -0,0 +1,41 @@
+/**
+ * Given an integer array nums and an integer k, return the kth largest element
+ * in the array
+ *
+ * Note that it is the kth largest element in the sorted order, not the kth
+ * distinct element.
+ *
+ * Can you solve it without sorting?
+ *
+ * Constraints:
+ *    1 <= k <= nums.length <= 10^5
+ *    -10^4 <= nums[i] <= 10^4
+ */
+
+import { MinPriorityQueue } from '@datastructures-js/priority-queue'
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findKthLargest = (nums, k) => {
+  const queue = new MinPriorityQueue({ priority: (num) => num })
+
+  for (const num of nums) {
+    queue.enqueue(num)
+    if (queue.size() > k) queue.dequeue()
+  }
+
+  return queue.dequeue().element
+}
+
+let nums = [3, 2, 1, 5, 6, 4]
+let k = 2
+// Expected: 5
+
+// nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+// k = 4
+// Expected: 4
+
+console.log(findKthLargest(nums, k))
