ad3ce8dd17b59672c58e74bfcc356b68db627476
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-08-18 00:21:07 -0300

    1615. Maximal network rank

---
+++ 1615-maximal-network-rank.js
@@ -0,0 +1,65 @@
+/**
+ * There is an infrastructure of n cities with some number of roads connecting
+ * these cities. Each roads[i] = [ai, bi] indicates that there is a
+ * bidirectional road between cities ai and bi.
+ *
+ * The network rank of two different cities is defined as the total number of
+ * directly connected roads to either city. If a road is directly connected to
+ * both cities, it is only counted once.
+ *
+ * The maximal network rank of the infrastructure is the maximum network rank of
+ * all pairs of different cities.
+ *
+ * Given the integer n and the array roads, return the maximal network rank of
+ * the entire infrastructure.
+ *
+ * Constraints:
+ *    2 <= n <= 100
+ *    0 <= roads.length <= n * (n - 1) / 2
+ *    roads[i].length == 2
+ *    0 <= ai, bi <= n-1
+ *    ai != bi
+ *    Each pair of cities has at most one road connecting them.
+ */
+
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
+const maximalNetworkRank = (n, roads) => {
+  const adjs = new Array(n).fill().map((_) => [])
+  for (const [u, v] of roads) {
+    adjs[u].push(v)
+    adjs[v].push(u)
+  }
+
+  let maxRank = 0
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+      rank = adjs[i].length + adjs[j].length
+      if (adjs[i].includes(j)) rank -= 1
+      maxRank = Math.max(maxRank, rank)
+    }
+  }
+
+  return maxRank
+}
+
+n = 4
+// prettier-ignore
+roads = [ [0, 1], [0, 3], [1, 2], [1, 3], ]
+// Expected: 4
+
+// n = 5
+// // prettier-ignore
+// roads = [ [0, 1], [0, 3], [1, 2], [1, 3], [2, 3], [2, 4], ]
+// Expected: 5
+
+// n = 8
+// // prettier-ignore
+// roads = [ [0, 1], [1, 2], [2, 3], [2, 4], [5, 6], [5, 7], ]
+// Expected: 5
+
+console.log(maximalNetworkRank(n, roads))
