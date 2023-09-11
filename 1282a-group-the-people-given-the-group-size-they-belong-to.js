f8b7b5dcaaf95d9b658bdbf55391311bbfc3b263
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-09-11 00:00:02 -0300

    1282. Group the people given the group size they belong to

---
+++ 1282a-group-the-people-given-the-group-size-they-belong-to.js
@@ -0,0 +1,53 @@
+/**
+ * There are n people that are split into some unknown number of groups. Each
+ * person is labeled with a unique ID from 0 to n - 1.
+ *
+ * You are given an integer array groupSizes, where groupSizes[i] is the size of
+ * the group that person i is in. For example, if groupSizes[1] = 3, then person
+ * 1 must be in a group of size 3.
+ *
+ * Return a list of groups such that each person i is in a group of size
+ * groupSizes[i].
+ *
+ * Each person should appear in exactly one group, and every person must be in a
+ * group. If there are multiple answers, return any of them. It is guaranteed
+ * that there will be at least one valid solution for the given input.
+ *
+ * Constraints:
+ *    groupSizes.length == n
+ *    1 <= n <= 500
+ *    1 <= groupSizes[i] <= n
+ */
+
+import _ from 'lodash'
+
+/**
+ * @param {number[]} groupSizes
+ * @return {number[][]}
+ */
+const groupThePeople = (groupSizes) => {}
+
+const funcs = [groupThePeople]
+
+const data = [
+  [
+    [3, 3, 3, 3, 3, 1, 3],
+    [[5], [0, 1, 2], [3, 4, 6]],
+  ],
+  // [
+  //   [2, 1, 3, 3, 3, 2],
+  //   [[1], [0, 5], [2, 3, 4]],
+  // ],
+]
+
+for (const func of funcs) {
+  for (const [groupSizes, expected] of data) {
+    const output = func(groupSizes)
+      .map((p) => p.sort((a, b) => a - b))
+      .sort((a, b) =>
+        a.length === b.length ? a[0] - b[0] : a.length - b.length
+      )
+
+    console.log(_.isEqual(output, expected))
+  }
+}
