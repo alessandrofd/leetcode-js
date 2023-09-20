4917c3b2f896c685a00aef5701830267e932b003
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-09-20 00:55:43 -0300

    1658. Minimum operations to reduce X to zero

--- 1658-minimum-operations-to-reduce-x-to-zero.js
+++ 1658-minimum-operations-to-reduce-x-to-zero.js
@@ -1,15 +1,34 @@
-/* Ao invés de tentar as combinações no início e no final no array,
- * nós tentamos descobrir qual o maior subarray cuja somatória é a diferença
- * enter o valor alvo (parâmetro x) e a somatória de todos os elementos do
- * array (variável y). Para tanto o primeiro passo é transformar o array
- * fornecido em um somatório dos elementos do array original. Assim utiliza-
- * mos dois ponteiros para demarcar o subarray e o seu somatório será igual
- * a posição final do subarray, indicado pelo segundo ponteiro (variável j)
- * menos os valores descartados que será igual à posição anterior à posição
- * inicial do subarray, indicada pelo primeiro ponteiro (variável i)
+/**
+ * You are given an integer array nums and an integer x. In one operation, you
+ * can either remove the leftmost or the rightmost element from the array nums
+ * and subtract its value from x. Note that this modifies the array for future
+ * operations.
+ *
+ * Return the minimum number of operations to reduce x to exactly 0 if it is
+ * possible, otherwise, return -1.
+ *
+ * Constraints:
+ *    1 <= nums.length <= 10^5
+ *    1 <= nums[i] <= 10^4
+ *    1 <= x <= 10^9
  */
 
+/**
+ * @param {number[]} nums
+ * @param {number} x
+ * @return {number}
+ */
 const minOperations = (nums, x) => {
+  /* Ao invés de tentar as combinações no início e no final no array,
+   * nós tentamos descobrir qual o maior subarray cuja somatória é a diferença
+   * entre o valor alvo (parâmetro x) e a somatória de todos os elementos do
+   * array (variável y). Para tanto o primeiro passo é transformar o array
+   * fornecido em um somatório dos elementos do array original. Assim utiliza-
+   * mos dois ponteiros para demarcar o subarray e o seu somatório será igual
+   * a posição final do subarray, indicado pelo segundo ponteiro (variável j)
+   * menos os valores descartados que será igual à posição anterior à posição
+   * inicial do subarray, indicada pelo primeiro ponteiro (variável i)
+   */
   const len = nums.length
   for (let i = 1; i < len; i++) nums[i] = nums[i] + nums[i - 1]
   let y = nums[len - 1] - x
@@ -25,16 +44,19 @@ const minOperations = (nums, x) => {
   return best > 0 ? len - best : -1
 }
 
-// let nums = [1, 1, 4, 2, 3]
-// let x = 5
-//Output: 2
+// prettier-ignore
+const funcs = [ 
+  minOperations
+]
 
-// let nums = [5, 6, 7, 8, 9]
-// let x = 4
-// Output: -1
+const data = [
+  [[1, 1, 4, 2, 3], 5, 2],
+  [[5, 6, 7, 8, 9], 4, -1],
+  [[3, 2, 20, 1, 1, 3], 10, 5],
+]
 
-let nums = [3, 2, 20, 1, 1, 3]
-let x = 10
-//Output: 5
-
-console.log(minOperations(nums, x))
+for (const func of funcs) {
+  for (const [nums, x, expected] of data) {
+    console.log(func(nums, x) === expected)
+  }
+}
---
+++ 1658a-minimum-operations-to-reduce-x-to-zero.js
@@ -0,0 +1,114 @@
+/**
+ * You are given an integer array nums and an integer x. In one operation, you
+ * can either remove the leftmost or the rightmost element from the array nums
+ * and subtract its value from x. Note that this modifies the array for future
+ * operations.
+ *
+ * Return the minimum number of operations to reduce x to exactly 0 if it is
+ * possible, otherwise, return -1.
+ *
+ * Constraints:
+ *    1 <= nums.length <= 10^5
+ *    1 <= nums[i] <= 10^4
+ *    1 <= x <= 10^9
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} x
+ * @return {number}
+ */
+const minOperations_map = (nums, x) => {
+  /* 
+  Ao invés de tentar as combinações no início e no final no array,
+  nós tentamos descobrir qual o maior subarray cuja somatória de seus valores é 
+  a diferença entre o parâmetro x e a somatória de todos os elementos do vetor.
+  Este será o nosso valor alvo.
+
+  Percorremos o vetor totalizando os seus valores. Como o percorremos da 
+  esquerda para a direita, podemos considerar que cada iteração corresponde a 
+  uma supressão, cada vez menor, de elementos à direita. Utilizamos uma mapa 
+  para armazenar o índice das somas parciais do vetor. A cada passo testamos se 
+  já houve uma soma parcial anterior que quando subtraída da soma atual 
+  corresponde ao valor alvo. Caso o teste seja possitivo, o valor armazenado no 
+  mapa corresponderá aos elementos à esquerda a serem eliminados. Por outro 
+  lado, o próprio índice da iteração marca o limite do subarray, ou seja, os 
+  elementos à direita não devem ser considerados para que obtenhamos o valor 
+  desejado. 
+
+  Como é possível que haja mais de um subarray cuja soma de valores seja igual 
+  ao valor que procuramos, devemos percorrer todo vetor. Toda vez que acharmos 
+  um subarray válido, devemos testar se o seu comprimento é maior que os dos 
+  demais subarrays válidos. O seu comprimento será igual ao índice da iteração 
+  menos os elementos à esquerda a serem eliminado - valor armazenado no mapa.
+  */
+  const n = nums.length
+  const total = nums.reduce((acc, n) => acc + n)
+  if (total === x) return n
+
+  const target = total - x
+  const indexBySum = new Map()
+  indexBySum.set(0, -1)
+
+  let sum = 0
+  let maxLen = 0
+  for (let i = 0; i < n; i++) {
+    sum += nums[i]
+    if (indexBySum.has(sum - target))
+      maxLen = Math.max(maxLen, i - indexBySum.get(sum - target))
+
+    indexBySum.set(sum, i)
+  }
+
+  return maxLen === 0 ? -1 : n - maxLen
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} x
+ * @return {number}
+ */
+const minOperations_pointers = (nums, x) => {
+  /* 
+  Aqui usamos explicitamente dois ponteiros que indicarão os limites do 
+  subarray. Avançamos primeiramente o ponteiro da direita e assim que o 
+  somatório do subarray extrapolar o valor alvo, avançamos o ponteiro da 
+  esquerda. 
+  */
+  const n = nums.length
+  const total = nums.reduce((acc, n) => acc + n)
+  if (total === x) return n
+
+  const target = total - x
+  target
+
+  let sum = 0
+  let maxLen = 0
+  for (let left = 0, right = 0; right < n; right++) {
+    sum += nums[right]
+    while (left <= right && sum > target) sum -= nums[left++]
+    if (sum === target) {
+      maxLen = Math.max(maxLen, right - left + 1)
+    }
+  }
+
+  return maxLen === 0 ? -1 : n - maxLen
+}
+
+// prettier-ignore
+const funcs = [ 
+  // minOperations_map,
+  minOperations_pointers,
+]
+
+const data = [
+  [[1, 1, 4, 2, 3], 5, 2],
+  [[5, 6, 7, 8, 9], 4, -1],
+  [[3, 2, 20, 1, 1, 3], 10, 5],
+]
+
+for (const func of funcs) {
+  for (const [nums, x, expected] of data) {
+    console.log(func(nums, x) === expected)
+  }
+}
