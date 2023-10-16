de7a33f769f694e486ce2a32aeda5b68e12f7665
Alessandro França Dantas <alessandro.dantas@economia.gov.br>
2023-10-16 10:46:44 -0300

    119. Pascal's triangle II

---
+++ 119-pascals-triangle-ii.js
@@ -0,0 +1,51 @@
+/**
+ * Given an integer rowIndex, return the rowIndexth (0-indexed) row of the
+ * Pascal's triangle.
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it.
+ *
+ * Constraints:
+ *    0 <= rowIndex <= 33
+ */
+
+import _ from 'lodash'
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+const getRow = (rowIndex) => {
+  if (rowIndex === 0) return [1]
+  if (rowIndex === 1) return [1, 1]
+
+  let prevRow = [1, 1]
+  for (let i = 1; i < rowIndex; i++) {
+    const row = [1]
+    for (let j = 1; j < prevRow.length; j++) {
+      row.push(prevRow[j] + prevRow[j - 1])
+    }
+    row.push(1)
+    prevRow = row
+  }
+
+  return prevRow
+}
+
+// prettier-ignore
+const funcs = [
+  getRow,
+]
+
+const data = [
+  [3, [1, 3, 3, 1]],
+  [0, [1]],
+  [1, [1, 1]],
+]
+
+for (const func of funcs) {
+  for (const [rowIndex, expected] of data) {
+    const output = func(rowIndex)
+    console.log(_.isEqual(output, expected))
+  }
+}
